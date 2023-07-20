2dd89b8fb2f4ac029aa9b49b1941ea014906a512
bhathiknk <bhathikanileshkavindya@gmail.com>
2023-07-20 23:11:42 +0530

    based files

---
+++ src/views/category/AddCategory.vue
@@ -0,0 +1,39 @@
+<template>
+ 
+   <div class="ccontainer">
+    <div class="row">
+     <div class="text-center">
+        <h3> Add Category</h3>
+     </div>
+    </div>
+   </div>
+   <div class="row">
+    <form>
+        <div class="form-group">
+            <label>Category name</label>
+            <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label>Category Description</label>
+            <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+            <label>Category Image</label>
+            <input type="text" class="form-control" />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+   </div>
+</template>
+
+<script> 
+export default{
+    setup(){
+
+    },
+}
+</script>
+
+<style>
+
+</style>
