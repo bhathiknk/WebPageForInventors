0bb4fbed126abf30f926d13e2cb21e0bb181c32c
bhathiknk <bhathikanileshkavindya@gmail.com>
2023-07-21 21:23:56 +0530

    category list and add category feature added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddCategory from '../views/category/AddCategory.vue'
+import Category from '../views/category/Category.vue'
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path:'/admin/category/add',
     name:'AddCategory',
     component:AddCategory
+  }, 
+  {
+    path:'/admin/category',
+    name:'Category',
+    component:Category
   }
 ]
 
--- src/views/category/AddCategory.vue
+++ src/views/category/AddCategory.vue
@@ -1,39 +1,76 @@
 <template>
- 
-   <div class="container">
-    <div class="row">
-     <div class="text-center">
-        <h3> Add Category</h3>
-     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h3 class="pt-3">Add Category</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+          <form>
+            <div class="form-group">
+              <label>Name</label>
+              <input type="text" class="form-control" v-model="categoryName" />
+            </div>
+            <div class="form-group">
+              <label>Description</label>
+              <textarea type="text" class="form-control" v-model="description" />
+            </div>
+            <div class="form-group">
+              <label>Image</label>
+              <input type="text" class="form-control" v-model="imageUrl" />
+            </div>
+            <button type="button" class="btn btn-primary" @click="addCategory">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div class="col-3"></div>
+      </div>
     </div>
-   <div class="row">
-    <form>
-        <div class="form-group">
-            <label>Category name</label>
-            <input type="text" class="form-control" />
-        </div>
-        <div class="form-group">
-            <label>Category Description</label>
-            <input type="text" class="form-control" />
-        </div>
-        <div class="form-group">
-            <label>Category Image</label>
-            <input type="text" class="form-control" />
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-   </div>
-   </div>
-</template>
-
-<script> 
-export default{
-    setup(){
-
+  </template>
+  <script>
+  const axios = require("axios");
+  const sweetalert = require("sweetalert");
+  export default {
+    data() {
+      return {
+        categoryName: "",
+        description: "",
+        imageUrl: "",
+      };
     },
-}
-</script>
-
-<style>
-
-</style>
+    methods: {
+      addCategory() {
+        console.log(this.categoryName, this.description);
+        const newCategory = {
+          categoryName: this.categoryName,
+          description: this.description,
+          imageUrl: this.imageUrl,
+        };
+  
+        const baseURL = "https://limitless-lake-55070.herokuapp.com";
+  
+        axios({
+          method: "post",
+          url: `${baseURL}/category/create`,
+          data: JSON.stringify(newCategory),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then(() => {
+            sweetalert({
+              text: "Category added successfully",
+              icon: "success",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+    },
+  };
+  </script>
+  <style scoped></style>
